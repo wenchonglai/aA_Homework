6271b3b707d712b60c62ae71d5e123c69a307100
Wenchong Lai <wenchonglai@gmail.com>
2021-02-18 23:36:12 -0800

    Finish W5D5 Homework - "N Plus One"

---
+++ W5D5/n-plus-1-buster/app/models/artist.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Artist < ApplicationRecord
+  has_many :albums,
+    class_name: 'Album',
+    foreign_key: :artist_id,
+    primary_key: :id
+
+  def n_plus_one_tracks
+    albums = self.albums
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.tracks.length
+    end
+
+    tracks_count
+  end
+
+  def better_tracks_query
+    # TODO: your code here
+    albums = self.albums.includes(:title, :tracks)
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.tracks.length
+    end
+
+    tracks_count
+  end
+end
