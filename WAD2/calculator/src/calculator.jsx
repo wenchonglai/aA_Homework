284b3de71f1cec6b0701031cfdb4b741400fff53
Wenchong Lai <wenchonglai@gmail.com>
2021-03-23 08:56:12 -0700

    Add feature for the clear button

--- WAD2/calculator/src/calculator.jsx
+++ WAD2/calculator/src/calculator.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import ReactDOM from 'react-dom'
 
 export default function Calculator(props){
   let [currVal, setCurrVal] = React.useState(0);
+  let input1 = React.useRef(undefined);
+  let input2 = React.useRef(undefined);
   
   function handleClick(e){
     e.preventDefault();
@@ -11,13 +13,17 @@ export default function Calculator(props){
 
     let val1 = Number( formData.get('input1') ) || 0;
     let val2 = Number( formData.get('input2') ) || 0;
+
     
     switch (e.target.value){
       case '+': setCurrVal(val1 + val2); break;
       case '-': setCurrVal(val1 - val2); break;
       case '*': setCurrVal(val1 * val2); break;
       case '/': setCurrVal(val1 / val2); break;
-      case 'clear': setCurrVal(5); break;
+      case 'clear': {
+        input1.current.value = '';
+        input2.current.value = '';
+      }; break;
     }
   }
 
@@ -25,17 +31,16 @@ export default function Calculator(props){
     <div className="calculator">
       <h1>{currVal}</h1>
       <form onClick={handleClick}>
-        <input type="text" name="input1"/>
-        <input type="text" name="input2"/>
+        <input type="text" ref={input1} name="input1" val=""/>
+        <input type="text" ref={input2} name="input2"/>
 
         <input type="submit" value="clear"/>
 
         <br/>
 
-        <input type="submit" value="+"/>
-        <input type="submit" value="-"/>
-        <input type="submit" value="*"/>
-        <input type="submit" value="/"/>
+        {['+', '-', '*', '/'].map( symb => 
+          <input type="submit" value={symb} key={symb}/>
+        )}
       </form>
     </div>
   )
