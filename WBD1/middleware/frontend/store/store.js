efaf1c1722461d2d3c3ead2164df7ebd007e2063
Wenchong Lai <wenchonglai@gmail.com>
2021-03-29 08:40:32 -0700

    Finish WBD1 homework

---
+++ WBD1/middleware/frontend/store/store.js
@@ -0,0 +1,20 @@
+import { createStore } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+import {applyMiddleware} from 'redux';
+
+const addLoggingToDispatch = store => next => action => {
+    console.log(store.getState());
+    console.log(action);
+    next(action);
+    console.log(store.getState());
+}
+
+const configureStore = (preloadedState = {}) => {
+  const store = createStore(rootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
+  store.subscribe(() => {
+    localStorage.state = JSON.stringify(store.getState());
+  });
+  return store;
+}
+
+export default configureStore;
